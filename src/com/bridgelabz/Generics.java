0d59a86252b9fdca7735fd0c1ef3b67a616afbc9
Supriya Bhurle <supriyabhurle28@gmail.com>
2022-12-28 14:44:45 +0530

    given three String find the maximum

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,6 +1,23 @@
 package com.bridgelabz;
 
 public class Generics {
+
+    static void maximum_variables(String str1, String str2, String str3){
+        if(str1.compareTo(str2) > 0){
+            if(str1.compareTo(str3) > 0){
+                System.out.println( str1 + " is Maximum of three string ");
+            }else {
+                System.out.println( str3 + " is Maximum of three string ");
+            }
+        }else{
+            if(str2.compareTo(str3) > 0){
+                System.out.println( str2 + " is Maximum of three string ");
+            }else{
+                System.out.println( str3 + " is Maximum of three string ");
+            }
+        }
+    }
+
     static void maximum_variables(Integer num1, Integer num2, Integer num3){
         if(num1.compareTo(num2) > 0){
             if(num1.compareTo(num3) > 0){
@@ -18,6 +35,7 @@ public class Generics {
     }
 
     public static void main(String[] args) {
+        maximum_variables("mango","Strawberry","orange");
         maximum_variables(38,700,60);
     }
 }
